d3e2cbc7f341a7c9311501f256c2b99522ad0d76
Joseph Saw <joesawtw@gmail.com>
2019-06-15 19:00:34 +0800

    hover & click effects added to fav icon for beer card

--- src/components/BeerCard.js
+++ src/components/BeerCard.js
@@ -3,35 +3,115 @@ import React from 'react'
 import { faHeart as farHeart } from "@fortawesome/free-regular-svg-icons";
 import { faHeart as fasHeart } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Style from 'style-it';
 
 const Heart = ({ filled, onClick }) => {
 
     return (
         <div onClick={onClick} >
-            <FontAwesomeIcon icon={filled ? farHeart : fasHeart} />
+            <FontAwesomeIcon className="heartIcon" icon={filled ? fasHeart : farHeart} />
         </div>
     );
 };
 
 class BeerCard extends React.Component {
+    state = { fav: false };
+
+    handleClick = () => {
+        this.setState({ filled: !this.state.filled });
+    }
 
     render() {
         return (
-            <div style={styles.card}>
-                <div style={styles.imgHolder}>
-                    <img alt={this.props.beer.name} src={this.props.beer.image_url} />
-                </div>
-                <div style={styles.contentHolder}>
-                    <div style={styles.contentHeader}>
-                        <p style={{ fontWeight: '600' }}> {this.props.beer.name} </p>
-                        <Heart />
-                    </div>
+            <Style>
+                {`
+                    .heartIcon {
+                        color: red;
+                    }
 
-                    <div style={styles.contentDesc}>
-                        <p>{this.props.beer.description}</p>
+                    .heartIcon:hover {
+                        -webkit-animation: wobble-hor-bottom 0.8s both;
+	                    animation: wobble-hor-bottom 0.8s both;
+                    }
+
+                    @-webkit-keyframes wobble-hor-bottom {
+                        0%,
+                        100% {
+                          -webkit-transform: translateX(0%);
+                                  transform: translateX(0%);
+                          -webkit-transform-origin: 50% 50%;
+                                  transform-origin: 50% 50%;
+                        }
+                        15% {
+                          -webkit-transform: translateX(-10px) rotate(-3deg);
+                                  transform: translateX(-10px) rotate(-3deg);
+                        }
+                        30% {
+                          -webkit-transform: translateX(7.5px) rotate(3deg);
+                                  transform: translateX(7.5px) rotate(3deg);
+                        }
+                        45% {
+                          -webkit-transform: translateX(-7.5px) rotate(-3deg);
+                                  transform: translateX(-7.5px) rotate(-3deg);
+                        }
+                        60% {
+                          -webkit-transform: translateX(4.5px) rotate(1.2deg);
+                                  transform: translateX(4.5px) rotate(1.2deg);
+                        }
+                        75% {
+                          -webkit-transform: translateX(-3px) rotate(-0.6deg);
+                                  transform: translateX(-3px) rotate(-0.6deg);
+                        }
+                      }
+                      @keyframes wobble-hor-bottom {
+                        0%,
+                        100% {
+                          -webkit-transform: translateX(0%);
+                                  transform: translateX(0%);
+                          -webkit-transform-origin: 50% 50%;
+                                  transform-origin: 50% 50%;
+                        }
+                        15% {
+                            -webkit-transform: translateX(-10px) rotate(-3deg);
+                                    transform: translateX(-10px) rotate(-3deg);
+                          }
+                          30% {
+                            -webkit-transform: translateX(7.5px) rotate(3deg);
+                                    transform: translateX(7.5px) rotate(3deg);
+                          }
+                          45% {
+                            -webkit-transform: translateX(-7.5px) rotate(-3deg);
+                                    transform: translateX(-7.5px) rotate(-3deg);
+                          }
+                          60% {
+                            -webkit-transform: translateX(4.5px) rotate(1.2deg);
+                                    transform: translateX(4.5px) rotate(1.2deg);
+                          }
+                          75% {
+                            -webkit-transform: translateX(-3px) rotate(-0.6deg);
+                                    transform: translateX(-3px) rotate(-0.6deg);
+                          }
+                      }
+                      
+                   
+                `}
+
+                <div style={styles.card}>
+                    <div style={styles.imgHolder}>
+                        <img alt={this.props.beer.name} src={this.props.beer.image_url} />
+                    </div>
+                    <div style={styles.contentHolder}>
+                        <div style={styles.contentHeader}>
+                            <p style={{ fontWeight: '600' }}> {this.props.beer.name} </p>
+                            <Heart filled={this.state.filled} onClick={this.handleClick} />
+                        </div>
+
+                        <div style={styles.contentDesc}>
+                            <p>{this.props.beer.description}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </Style>
         );
     }
 }
@@ -56,7 +136,7 @@ const styles = {
     },
     contentHeader: {
         display: 'flex',
-        justifyContent: 'spaceBetween'
+        justifyContent: 'space-between'
     },
     contentDesc: {
         marginTop: '18px',
